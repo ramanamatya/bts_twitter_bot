3caaa07d35d6bd474f34d28b0db2330917fc0b46
raman <ramanamatya@gmail.com>
2020-10-17 19:29:38 +0545

    filter out replys tweet.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Twitter = new twit(config);
 //find latest tweet according the query 'q' in params
 const retweet = () => {
     const params = {
-        q: '#BTS OR @BTS_twt -filter:retweets',  // REQUIRED
+        q: '#BTS OR @BTS_twt -filter:retweets -filter:replies',  // REQUIRED
         result_type: 'recent',
         lang: 'en'
     };
