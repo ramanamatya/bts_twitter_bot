629b055a2f0cb061fa409e5ec32d93f337497157
raman <ramanamatya@gmail.com>
2020-10-17 19:11:40 +0545

    used or operator.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Twitter = new twit(config);
 //find latest tweet according the query 'q' in params
 const retweet = () => {
     const params = {
-        q: '#BTS, @BTS_twt',  // REQUIRED
+        q: '#BTS OR @BTS_twt',  // REQUIRED
         result_type: 'recent',
         lang: 'en'
     };
